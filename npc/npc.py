61addd28cf66d9a7e0750aaddb8a8cc0c88cafcc
Edward Abrams <zeroaltitude@gmail.com>
2018-08-04 23:44:53 -0700

    Update npc.py

--- npc/npc.py
+++ npc/npc.py
@@ -1,18 +1,5 @@
-#!/usr/bin/python
+class NPC(object):
 
-# beta class for NPCs
-# 
-
-class IterRegistry(type):
-    def __iter__(cls):
-        return iter(cls._NPCs)
-
-class NPC:
-
-    _NPCs = []
-
-    __metaclass__ = IterRegistry
-    
     def __init__(self, sn, name, health, desc):
         self._NPCs.append(self)
         self.sn = sn # int epoch:float Darknet iteration:float version:int id
@@ -22,32 +9,28 @@ class NPC:
         self.actions = [{'verb':'look','resp':desc, 'func':None}] # list of dict {verb, description, function to execute before reply
         self.inventory = [] # list of dict {name, desc, qty}
 
-    def __del__(self):
-        return
-
     def rename(self,name):
 	self.name = name
 
     def addAction(self,verb,resp,func):
         self.actions.append({'verb':verb,'resp':resp,'func':func})
 
-# Darknet 2018 infection interactions
-#
-# illcode {list of pathogens and injuries}
-#
+    # Darknet 2018 infection interactions
+    #
+    # illcode {list of pathogens and injuries}
+    #
     def infect(self, pathogen):
         self.health['illcode'].append(pathogen)
 
     def disinfect(self):
         self.health['illcode']=[0x00]
 
-# Darknet NPC generic gamification actions
-#
-# Boolean: health.hungry, health.thirsty
-# String: health.mood
-# Integer: health.hp (0 == dead)
-# Use NPC addAction and call these to have custom actions change NPC state
-
+    # Darknet NPC generic gamification actions
+    #
+    # Boolean: health.hungry, health.thirsty
+    # String: health.mood
+    # Integer: health.hp (0 == dead)
+    # Use NPC addAction and call these to have custom actions change NPC state
     def feed(self, item):
         self.health['hungry']=False
         self.take(item)
